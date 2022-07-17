4073e2874f827f874625890a4b76c92d243bcd5f
Fernando Tomlinson <WiredPulse@users.noreply.github.com>
2022-07-17 13:21:39 -0400

    Add files via upload

---
+++ Get-RAServiceCreation.ps1
@@ -0,0 +1,30 @@
+﻿function Get-RemoteAccessServiceCreation{
+<#
+    .SYNOPSIS
+        Parses the the offline Windows System log for the service installations of TeamViewer, AnyDesk, and ConnectWise. The returned data will include the timestamp, 
+        name of service, and executable associated with it.
+
+    .PARAMETER logFile
+        Location to offline Windows System log.
+
+    .EXAMPLE
+        Get-RemoteAccessServiceCreation -logfile c:\windows_system.evtx
+
+        Parses C:windows_system.evtx log and returns applicable data.
+#>
+    
+    [CmdletBinding()]
+    param(
+        $logfile
+    )    
+    
+    Get-WinEvent -FilterHashtable @{path="$logfile"; id='7045'} | 
+        where-object{$_.message -match "anydesk" -or $_.message -match "teamviewer" -or $_.message -match "screenconnect"} | 
+            Select-Object timecreated, 
+                ProcessID,
+                @{Label="ServiceName";Expression={$_.properties.value[0]}}, 
+                @{Label="ServiceFileName";Expression={$_.properties.value[1]}}, 
+                @{Label="ServiceType";Expression={$_.properties.value[2]}}, 
+                @{Label="ServiceStartupType";Expression={$_.properties.value[3]}}, 
+                @{Label="ServiceAccount";Expression={$_.properties.value[4]}} | format-table
+}
